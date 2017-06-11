94167400a2df8fd850e4621e9071b5523c8552f2
Xi Han <xihan94@ucla.edu>
2017-06-11 03:15:34 -0700

    Implement multiple generator

--- src/main/scala/trigsolver/MultipleGenerator.scala
+++ src/main/scala/trigsolver/MultipleGenerator.scala
@@ -6,9 +6,14 @@ class MultipleGenerator(val m:Int) extends Module {
   val io = IO(new Bundle {
     val d = Input(SInt(2.W))  // SD
     val a = Input(SInt((m + 1).W)) // S.XXXX if m = 4
-    val mult = Output(SInt((m + 2).W)) // SX.XXXX if m = 4
+    val mult = Output(SInt((m + 1).W)) // S.XXXX if m = 4
     val comp = Output(UInt(1.W)) // C - should complement?
   })
 
   // TODO: implement it!
+  val zeros = 0.asSInt((m + 1).W)
+  val aInv = io.a ^ -1.asSInt((m + 1).W)
+
+  io.mult := Mux(io.d(0), zeros, Mux(io.d(1), aInv, io.a))
+  io.comp := io.d(1)
 }
