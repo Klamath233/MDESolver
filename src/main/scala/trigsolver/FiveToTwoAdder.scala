df78e6958082e3c81b50f2d9538677c1bc632e37
Xi Han <xihan94@ucla.edu>
2017-06-11 02:34:15 -0700

    Add and update some definitions of modules

--- src/main/scala/trigsolver/FiveToTwoAdder.scala
+++ src/main/scala/trigsolver/FiveToTwoAdder.scala
@@ -9,8 +9,12 @@ class FiveToTwoAdder(val m:Int) extends Module {
     val i2 = Input(SInt((m + 3).W))  // SSX.XXXX left multiple
     val i3 = Input(SInt((m + 3).W))  // SSX.XXXX right multiple
     val i4 = Input(SInt((m + 3).W))  // SSD.000C d of the module itself concat with Cr or Cl
+    val cIn0 = Input(UInt(1.W))
+    val cIn1 = Input(UInt(1.W))
+    val cOut0 = Input(UInt(1.W))
+    val cOut1 = Input(UInt(1.W))
     val o0 = Output(SInt((m + 3).W)) // SSX.XXXX new sum
-    val o1 = Output(SInt((m + 3).W)) // SXX.XXX0 new carry
+    val o1 = Output(SInt((m + 4).W)) // SSXX.XXX0 new carry
   })
 
   // TODO: implement it!
---
+++ src/main/scala/trigsolver/FiveToTwoModule.scala
@@ -0,0 +1,19 @@
+package trigsolver
+
+import chisel3._
+
+class FiveToTwoModuel(val m:Int) extends Module {
+  val io = IO(new Bundle {
+    val i0 = Input(UInt(1.W))  
+    val i1 = Input(UInt(1.W))
+    val i2 = Input(UInt(1.W))
+    val i3 = Input(UInt(1.W))
+    val i4 = Input(UInt(1.W))
+    val cIn0 = Input(UInt(1.W))
+    val cIn1 = Input(UInt(1.W))
+    val cOut0 = Input(UInt(1.W))
+    val cOut1 = Input(UInt(1.W))
+    val o0 = Output(UInt(1.W))
+    val o1 = Output(UInt(1.W))
+  })
+}
