46a42dff7f4688d3b7e7d906f6f3fdbf1a764270
Xi Han <xihan94@ucla.edu>
2017-06-11 03:06:06 -0700

    Update BasicModule

--- src/main/scala/trigsolver/BasicModule.scala
+++ src/main/scala/trigsolver/BasicModule.scala
@@ -1,6 +1,7 @@
 package trigsolver
 
 import chisel3._
+import chisel3.util._
 
 class BasicModule(val m: Int) extends Module {
   val io = IO(new Bundle {
@@ -12,5 +13,32 @@ class BasicModule(val m: Int) extends Module {
     val dOut =  Output(SInt(2.W))       // SD
   })
 
-  // TODO: implement it!
+  val aLeftReg = RegInit(io.aLeft)
+  val aRightreg = RegInit(io.aRight)
+
+  val mgLeft = Module(new MultipleGenerator(m))
+  val mgRight = Module(new MultipleGenerator(m))
+
+  val sel = Module(new DigitSelector)
+  val dReg = RegNext(sel.io.d)
+  val adder = Module(new FiveToTwoAdder(m))
+
+  val sumReg = RegNext(adder.io.o0, io.b.pad(m + 3))
+  val carryReg = RegNext(adder.io.o1, 0.asSInt((m + 3).W))
+
+  mgLeft.io.d := io.dLeft
+  mgLeft.io.a := io.aLeft
+  mgRight.io.d := io.dRight
+  mgRight.io.a := io.aRight
+
+  adder.io.i0 := mgLeft.io.mult
+  adder.io.i1 := mgRight.io.mult
+  adder.io.i2 := sumReg
+  adder.io.i3 := Cat(carryReg(m + 1, 1), mgLeft.io.comp)
+  adder.io.i4 := Cat(dReg.pad(3), mgRight.io.comp.pad(m))
+
+  sel.io.wcHat := carryReg(m + 2, m - 1)
+  sel.io.wsHat := sumReg(m + 2, m - 1)
+
+  io.dOut := sel.io.d
 }
