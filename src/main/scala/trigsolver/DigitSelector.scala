9e82b71a5d6cbb2361d6e80882cd7b41f8c5adf9
Xi Han <xihan94@ucla.edu>
2017-06-11 02:51:12 -0700

    Update interface of digit selector to carry save form

--- src/main/scala/trigsolver/DigitSelector.scala
+++ src/main/scala/trigsolver/DigitSelector.scala
@@ -4,7 +4,8 @@ import chisel3._
 
 class DigitSelector extends Module {
   val io = IO(new Bundle {
-    val vhat = Input(SInt(3.W)) // SX.X truncated v
+    val wcHat = Input(SInt(4.W))
+    val wsHat = Input(SInt(4.W))
     val d = Output(SInt(2.W))   // SD
   })
 
